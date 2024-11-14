57466f2db9a5b8c34309b7eb84849ba512ba8dcb
JakMal02 <jacob.malicki02@gmail.com>
2024-11-14 17:40:54 +0100

    dodano prompt i pytanie o uruchomienie

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ import os
 load_dotenv
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+messagees = [
+    {"role": "system", "content": "You are helpful assistant"},
+]
+
+artykul = open("tresc_artykulu.txt", "r")
+prompt = open("prompt.txt", "r")
+text = prompt.read() + "\n" + artykul.read()
+
+print("Czy chcesz przeksztalcic tresc artykulu z pliku na HTML gotowy do wklejenia w sekcji <body>? \t t/n")
+odp = input()
+
+if (odp == "n"):
+    print("Przerywam dzialanie programu")
+    exit()
+
+if (odp == "t"):
